bbf30d6bd97cee609abc9ea59e3aa75b46fdcf94
Beksultan Aitkazy <230599@astanait.edu.kz>
2023-12-23 22:49:25 +0600

    1st prototype of solution to the Assignment 1

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -1,4 +1,5 @@
 import models.Point;
+import models.Shape;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -7,12 +8,16 @@ import java.util.Scanner;
 public class MyApplication {
     public static void main(String[] args) throws FileNotFoundException {
         // implicit casting byte > short > int > long > float > double
-        File file = new File("C:\\Users\\BEKSULTAN\\IdeaProjects\\untitled\\src\\Input");
+        File file = new File("C:\\Users\\BEKSULTAN\\IdeaProjects\\assignment1\\src\\source");
         Scanner sc = new Scanner(file);
+        Shape shape = new Shape();
         while(sc.hasNextLine()){
-            Point p = new Point(sc.nextDouble(), sc.nextDouble());
+            Point point = new Point(sc.nextDouble(), sc.nextDouble());
+            shape.addPoint(point);
 
         }
-
+        System.out.println(shape.calculatePerimeter());
+        System.out.println(shape.getLongestSide());
+        System.out.println(shape.getAverageSide());
     }
 }
